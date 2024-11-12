19c6c7cb0abae7c608b35d9dec080658a4e6f10a
TomasBasualdo1 <116367430+TomasBasualdo1@users.noreply.github.com>
2024-11-12 11:58:45 -0300

    clases Actor/Grupo completas

--- src/main/java/org/example/model/Actor.java
+++ src/main/java/org/example/model/Actor.java
@@ -1,7 +1,32 @@
 package org.example.model;
 
 public class Actor {
-    private String nombre;
-    private String apellido;
-    public Grupo getGrupo() {}
+    private final String nombre;
+    private final String apellido;
+    private Grupo grupo;
+
+    public Actor(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+	public Grupo getGrupo() {
+		return grupo;
+	}
+
+	public void setGrupo(Grupo grupo) {
+		this.grupo = grupo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public String getNombreCompleto() {
+		return nombre + " " + apellido;
+	}
 }
--- src/main/java/org/example/model/Grupo.java
+++ src/main/java/org/example/model/Grupo.java
@@ -3,11 +3,34 @@ package org.example.model;
 import java.util.List;
 
 public class Grupo {
-    private String nombre;
+    private final String nombre;
     private List<Actor> actores;
 
+    public Grupo(String nombre, List<Actor> actores) {
+        this.nombre = nombre;
+        this.actores = actores;
+        setGrupoForActores();
+    }
 
+    private void setGrupoForActores() {
+      if (actores != null) {
+        for (Actor actor : actores) {
+            actor.setGrupo(this);
+        }
+      }
+    }
+
+    public void addActor(Actor actor) {
+      if (actor != null) {
+        actores.add(actor);
+        actor.setGrupo(this);
+      }
+    }
+    public String getNombre() { return nombre; }
 
     public List<Actor> getActores() { return List.copyOf(actores); }
-    public List<Funcion> getFunciones() {}
+
+    public List<Funcion> getFunciones() {
+      throw new UnsupportedOperationException("No implementado");
+    }
 }
